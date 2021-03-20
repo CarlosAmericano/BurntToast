7c0682496630131fbe450f25e0ed75c3668a8226
Josh King <windosbk+github@gmail.com>
2021-03-20 21:52:54 +1300

    Text refinement

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# To Do List (So I don't forget...)
+
+[ ] Capture error when adding too much text and make more user friendly (and warning?)
--- src/Public/Add-BTText.ps1
+++ src/Public/Add-BTText.ps1
@@ -1,6 +1,7 @@
 function Add-BTText {
     <#
         .SYNOPSIS
+        Add text to the toast.
 
         .DESCRIPTION
 
@@ -22,16 +23,38 @@ function Add-BTText {
                    ValueFromPipeline)]
         [Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder] $ContentBuilder,
 
+        # Custom text to display on the tile.
         [Parameter(Mandatory,
                    Position = 0)]
         [ValidateNotNull()]
-        [string[]] $Text
+        [string[]] $Text,
+
+        # The maximum number of lines the text element is allowed to display.
+        [ValidateRange(1,4)]
+        [int] $MaxLines,
+
+        # The target locale of the XML payload, specified as a BCP-47 language tags such as "en-US" or "fr-FR".
+        # The locale specified here overrides any other specified locale, such as that in binding or visual.
+        [string] $Language
     )
 
     begin {}
     process {
+        $paraStyle    = $null
+        $paraWrap     = $null
+        $paraMaxLines = if ($MaxLines) {$MaxLines} else {$null}
+        $paraMinLines = $null
+        $paraAlign    = $null
+        $paraLanguage = if ($Language) {$Language} else {$null}
+
         foreach ($Line in $Text) {
-            $null = $ContentBuilder.AddText($Line)
+            $ContentBuilder.AddText($Line,
+                                    $paraStyle,
+                                    $paraWrap,
+                                    $paraMaxLines,
+                                    $paraMinLines,
+                                    $paraAlign,
+                                    $paraLanguage)
         }
     }
     end {}
