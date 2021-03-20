c183d36f06cb61b4140c11645ab14aed3f8a204f
Josh King <windosbk+github@gmail.com>
2021-03-20 19:19:25 +1300

    Borrowing help from community toolkit

--- src/Public/Add-BTAppLogo.ps1
+++ src/Public/Add-BTAppLogo.ps1
@@ -1,6 +1,7 @@
 function Add-BTAppLogo {
     <#
         .SYNOPSIS
+        Override the app logo with custom image of choice that will be displayed on the toast.
 
         .DESCRIPTION
 
@@ -22,13 +23,16 @@ function Add-BTAppLogo {
                    ValueFromPipeline)]
         [Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder] $ContentBuilder,
 
+        # The URI of the image. Can be from your application package, application data, or the internet. Internet images must be less than 200 KB in size.
         [Parameter(Position = 0)]
         [ValidateNotNullOrEmpty()]
         [Alias('Uri')]
         [Uri] $Source = $Script:DefaultImage,
 
+        # Specify how the image should be cropped.
         [Microsoft.Toolkit.Uwp.Notifications.AdaptiveImageCrop] $Crop,
 
+        # A description of the image, for users of assistive technologies.
         [string] $AlternateText
     )
 
@@ -40,6 +44,8 @@ function Add-BTAppLogo {
             } else {
                 $null = $ContentBuilder.AddAppLogoOverride($Source, $Crop)
             }
+        } elseif ($AlternateText) {
+            $null = $ContentBuilder.AddAppLogoOverride($Source, 'Default', $AlternateText)
         } else {
             $null = $ContentBuilder.AddAppLogoOverride($Source)
         }
