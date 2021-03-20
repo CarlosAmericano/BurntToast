f8d75049927456410476d06d934d7dcf390dd197
Josh King <windosbk+github@gmail.com>
2021-03-20 22:05:07 +1300

    Image refinement

--- src/Public/Add-BTAppLogo.ps1
+++ src/Public/Add-BTAppLogo.ps1
@@ -23,7 +23,8 @@ function Add-BTAppLogo {
                    ValueFromPipeline)]
         [Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder] $ContentBuilder,
 
-        # The URI of the image. Can be from your application package, application data, or the internet. Internet images must be less than 200 KB in size.
+        # The URI of the image. Can be from your application package, application data, or the internet.
+        # Internet images must be less than 200 KB in size.
         [Parameter(Position = 0)]
         [ValidateNotNullOrEmpty()]
         [Alias('Uri')]
@@ -33,22 +34,30 @@ function Add-BTAppLogo {
         [Microsoft.Toolkit.Uwp.Notifications.AdaptiveImageCrop] $Crop,
 
         # A description of the image, for users of assistive technologies.
-        [string] $AlternateText
+        [string] $AlternateText,
+
+        # Specify whether Windows is allowed to append a query string to the image URI supplied in the Tile notification.
+        [switch] $AddImageQuery,
+
+        [switch] $IgnoreCache
     )
 
     begin {}
     process {
-        if ($Crop) {
-            if ($AlternateText) {
-                $null = $ContentBuilder.AddAppLogoOverride($Source, $Crop, $AlternateText)
-            } else {
-                $null = $ContentBuilder.AddAppLogoOverride($Source, $Crop)
-            }
-        } elseif ($AlternateText) {
-            $null = $ContentBuilder.AddAppLogoOverride($Source, 'Default', $AlternateText)
+        $paramCrop    = if ($Crop) {$Crop} else {'Default'}
+        $paramAltText = if ($AlternateText) {$AlternateText} else {$null}
+        $paramQuery   = if ($AddImageQuery) {$AddImageQuery} else {$null}
+
+        $paramSrc = if ($IgnoreCache) {
+            Optimize-BTImageSource -Source $Source -ForceRefresh
         } else {
-            $null = $ContentBuilder.AddAppLogoOverride($Source)
+            Optimize-BTImageSource -Source $Source
         }
+
+        $null = $ContentBuilder.AddAppLogoOverride($paramSrc,
+                                                   $paramCrop,
+                                                   $paramAltText,
+                                                   $paramQuery)
     }
     end {}
 }
