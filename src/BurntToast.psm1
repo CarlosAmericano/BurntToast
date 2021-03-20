412308a6397b78b32ddfdeec6f2f767ce475a0f6
Josh King <windosbk+github@gmail.com>
2021-03-20 18:01:12 +1300

    1.0.0 simplification?

--- src/BurntToast.psm1
+++ src/BurntToast.psm1
@@ -3,11 +3,15 @@ $WinMajorVersion = (Get-CimInstance -ClassName Win32_OperatingSystem -Property V
 if ($WinMajorVersion -ge 10) {
     $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
     $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-    $Library = @( Get-ChildItem -Path $PSScriptRoot\lib\Microsoft.Toolkit.Uwp.Notifications\*.dll -Recurse -ErrorAction SilentlyContinue )
 
-    if ($IsWindows) {
-        $Library += @( Get-ChildItem -Path $PSScriptRoot\lib\Microsoft.Windows.SDK.NET\*.dll -Recurse -ErrorAction SilentlyContinue )
+    $Paths = $Library = if ($IsWindows) {
+        "$PSScriptRoot\lib\Microsoft.Toolkit.Uwp.Notifications\net5.0-windows10.0.17763\*.dll",
+        "$PSScriptRoot\lib\Microsoft.Windows.SDK.NET\*.dll"
+    } else {
+        "$PSScriptRoot\lib\Microsoft.Toolkit.Uwp.Notifications\net461\*.dll"
     }
+
+    $Library = @( Get-ChildItem -Path $Paths -Recurse -ErrorAction SilentlyContinue )
 
     # Add one class from each expected DLL here:
     $LibraryMap = @{
@@ -39,21 +43,10 @@ if ($WinMajorVersion -ge 10) {
         }
     }
 
-    $Script:ActionsSupported = 'System.Management.Automation.SemanticVersion' -as [type] -and
-        $PSVersionTable.PSVersion -ge [System.Management.Automation.SemanticVersion] '7.1.0-preview.4'
-
-    $Script:UnsupportedEvents = 'Toast events are only supported on PowerShell 7.1.0 and above. ' +
-        'Your notification will still be displayed, but the actions will be ignored.'
-
     Export-ModuleMember -Alias 'Toast'
     Export-ModuleMember -Function $Public.BaseName
 
-    # Register default AppId
-    New-BTAppId
-
-    if (-not $IsWindows) {
-        $null = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
-    }
 } else {
-    throw 'This version of BurntToast will only work on Windows 10. If you would like to use BurntToast on Windows 8, please use version 0.4'
+    throw 'This version of BurntToast will only work on Windows 10. ' +
+          'If you would like to use BurntToast on Windows 8, please use version 0.4'
 }
