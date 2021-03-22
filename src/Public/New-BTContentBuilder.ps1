0145287c001005db51a0a92b7bec3af40de3cb59
Josh King <windosbk+github@gmail.com>
2021-03-22 20:40:59 +1300

    A little help

--- src/Public/New-BTContentBuilder.ps1
+++ src/Public/New-BTContentBuilder.ps1
@@ -1,19 +1,29 @@
 function New-BTContentBuilder {
     <#
         .SYNOPSIS
+        Create a toast builder object.
 
         .DESCRIPTION
+        This New-BTContentBuilder function creates a new toast content builder obejct to construct a complete notification.
 
         .INPUTS
+        None. You cannot pipe objects to New-BTContentBuilder.
 
         .OUTPUTS
+        Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder
 
         .EXAMPLE
+        $Builder = New-BTContentBuilder
 
-        .NOTES
+        This example creates a new toast content builder object and storing it in a variable named $Builder.
+
+        .EXAMPLE
+        $Builder = Builder
+
+        This example creates a new toast content builder object using the Builder alias and storing it in a variable named $Builder.
 
         .LINK
-
+        https://docs.toastit.dev/commands/new-btcontentbuilder
     #>
 
     [Alias('Builder')]
